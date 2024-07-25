1f9b385243d7c69c691c5661012b0744bb13da3b
tutoupfx <3205727707@qq.com>
2024-07-25 11:16:30 +0800

    init:vitepress博客初始化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "个人博客",
+  description: "个人博客",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/DashingYoungMan' }
+    ]
+  }
+})
